cdd47b4e0971ac596e5c9b020796d3d08b1e6acc
Secretwolf98 <jordanluders@gmail.com>
2019-12-30 10:21:53 -0600

    Change.

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/lineage/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := degas
 
-# Inherit some common CM stuff.
+# Inherit some common LinegeOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -12,7 +12,7 @@ $(call inherit-product, device/samsung/degas/device_degas.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := degas
-PRODUCT_NAME := cm_degas
+PRODUCT_NAME := lineage_degas
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := degas
 PRODUCT_MANUFACTURER := samsung
